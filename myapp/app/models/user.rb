807a386c69836f76405089d5643aaedc5816f99e
Aaron1515 <aaron1515@gmail.com>
2016-04-01 15:26:30 -0700

    password hash working

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,6 +1,42 @@
+require 'bcrypt'
+
 class User
   include Mongoid::Document
+  include BCrypt
+
+  attr_accessor         :password, :password_confirmation
+  # attr_protected        :password_hash
   field :name, type: String
   field :email, type: String
-  field :password, type: String
+  field :password_hash, :type => String
+  field :content, type: Array
+
+  before_save :encrypt_password
+
+  def self.find_by_email(email)
+    first(conditions: { email: email })
+  end
+
+  def self.authenticate(email, password)
+    if password_correct?(email, password)
+      # Success!
+      true
+    else
+      # Failed! :(
+      false
+    end
+  end
+
+  def self.password_correct?(user_email, password)
+    user = find_by_email user_email
+    return if user.nil?
+    user_pass = Password.new(user.password_hash)
+    user_pass == password
+  end
+
+  protected
+
+  def encrypt_password
+    self.password_hash = Password.create(@password)
+  end
 end
