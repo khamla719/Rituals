6e553f5abe0a87600f5ae5c842681251de75e1d8
Aaron1515 <aaron1515@gmail.com>
2016-04-01 18:29:25 -0700

    getting ready for crud

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -11,34 +11,13 @@ class User
   field :password_hash, :type => String
   field :content, type: Array
 
-  # before_save :encrypt_password
+  # validates_presence_of :email, :message => "Email Address is Required."
+  # validates_uniqueness_of :email, :message => "Email Address Already In Use. Have You Forgot Your Password?"
+  # validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => "Please Enter a Valid Email Address."
+  # validates_acceptance_of :accept_terms, :allow_nil => false, :accept => true, :message => "Terms and Conditions Must Be Accepted."
+  # validates_length_of :password, :minimum => 8, :message => "Password Must Be Longer Than 8 Characters."
+  # validates_confirmation_of :password, :message => "Password Confirmation Must Match Given Password."
 
-  # def self.find_by_email(email)
-  #   first(conditions: { email: email })
-  # end
-
-  # def self.authenticate(email, password)
-  #   if password_correct?(email, password)
-  #     # Success!
-  #     true
-  #   else
-  #     # Failed! :(
-  #     false
-  #   end
-  # end
-
-  # def self.password_correct?(user_email, password)
-  #   user = find_by_email user_email
-  #   return if user.nil?
-  #   user_pass = Password.new(user.password_hash)
-  #   user_pass == password
-  # end
-
-  # protected
-
-  # def encrypt_password
-  #   self.password_hash = Password.create(@password)
-  # end
   def password
     @password ||= BCrypt::Password.new(password_hash)
   end
