5472c37faac422f3e31ee2671949decf10119373
Aaron1515 <aaron1515@gmail.com>
2016-04-02 12:31:58 -0700

    delete recipe and logout needs to be done

--- rituals/config/routes.rb
+++ rituals/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :recipes
   end
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
