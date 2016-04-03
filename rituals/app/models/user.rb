a6e7c5e82955728c5776e34cca5b72e892d1e1fd
Aaron1515 <aaron1515@gmail.com>
2016-04-03 13:58:43 -0700

    can remove and add family member

--- rituals/app/models/user.rb
+++ rituals/app/models/user.rb
@@ -22,11 +22,26 @@ class User < ActiveRecord::Base
     # binding.pry
     a = Membership.new(user_id: self.id, family_id: member.id)
     if a.save
-      p "goot it"
+      p "User to New member DONE!"
     end
     b = Membership.new(user_id: member.id, family_id: self.id)
     if b.save
-      p "got b done"
+      p "Member Can see User DONE!"
     end
   end
+
+  def remove_family(member)
+    # binding.pry
+    a = Membership.find_by(user_id: self.id, family_id: member.id)
+    if a.delete
+      p "Remove User to Family Member"
+    end
+    b = Membership.find_by(user_id: member.id, family_id: self.id)
+    if b.delete
+      p "Remove Family Member to User"
+    end
+  end
+
+
+
 end
