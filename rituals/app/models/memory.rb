e0721c035d7fb092ecdff4500c44e896392fe997
khamla719 <khamla719@yahoo.com>
2016-04-03 16:21:42 -0700

    Add method to display all public or private meories

--- rituals/app/models/memory.rb
+++ rituals/app/models/memory.rb
@@ -1,3 +1,11 @@
 class Memory < ActiveRecord::Base
   belongs_to :user
+
+  def self.public
+    Memory.where(["private = false"])
+  end
+
+  def self.private
+    Memory.where(["private = true"])
+  end
 end
