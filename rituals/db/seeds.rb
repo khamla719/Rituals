f80fa57b160a3a0fb3264af733e8c08f34e3b08b
Aaron1515 <aaron1515@gmail.com>
2016-04-03 15:28:15 -0700

    add privte/public memory

--- rituals/db/seeds.rb
+++ rituals/db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-10.times do
-  User.create(first_name: Faker::Name.name , last_name: Faker::Name.last_name, password: Faker::Internet.password, email: Faker::Internet.email)
-end
+# 10.times do
+#   User.create(first_name: Faker::Name.name , last_name: Faker::Name.last_name, password: Faker::Internet.password, email: Faker::Internet.email)
+# end
 
-20.times do
-  Memory.create(title: Faker::Hacker.noun, url: Faker::Placeholdit.image, description: Faker::Hipster.sentence, story: Faker::Hipster.paragraph, user_id: rand(0..10)  )
-end
+# 20.times do
+#   Memory.create(title: Faker::Hacker.noun, url: Faker::Placeholdit.image, description: Faker::Hipster.sentence, story: Faker::Hipster.paragraph, user_id: rand(0..10)  )
+# end
 
 User.create(first_name: "aaron", last_name: "lastname", password: "password", email: "aaron@aaron.com")
 User.create(first_name: "khamla", last_name: "lastname", password: "password", email: "khamla@khamla.com")
