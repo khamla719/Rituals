fccd03f83c5c58ed37d75db4edfbfc179ecd39f0
Aaron1515 <aaron1515@gmail.com>
2016-04-05 09:41:52 -0700

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def show
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
     # binding.pry
-    if @user && @user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:user][:email].downcase)
+    # binding.pry
+    if @user && @user.authenticate(params[:user][:password])
       session[:id] = @user.id
-      redirect_to user_path(@user.id)
+      redirect_to @user
     else
       # Create an error message.
       render 'new'
@@ -16,6 +19,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:id] = nil
-  redirect_to root_path
+    redirect_to root_path
   end
 end
